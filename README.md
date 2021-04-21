1080452457029c66b585aea1386bd004b56bb62e
elmerihyvonen <elmeri.hyvonen@gmail.com>
2021-04-21 12:43:10 +0300

    Stakes, balance system and menu added

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ Implementation of card game 21 (Blackjack). User interaction is handled by conso
 
 ## Rules
 
-* Game follows the rules and logic as described in here: [21 card game](https://en.wikipedia.org/wiki/Twenty-One_(card_game))
-* Game is not as sophisticated as modern blackjack game so splitting a pair is not possible.
 * Game is implemented by using only one deck of cards, each card is unique
 * Dealer will 'stay' at 17 or higher
 * Dealer first card is kept hidden till the end of the game
-
-
+* If player has blackjack he receives a double of his stake
+* If the round result is draw player keeps the stake
+* If player scores closer to 21 than dealer, dealer pays the amount of player's stake
+* If dealer scores closer to 21 than player, dealer receives the stake
+* Game is not as sophisticated as modern blackjack game so splitting a pair, double down and insurance are not possible. 
--- app/Main.hs
+++ app/Main.hs
@@ -10,50 +10,96 @@ import Text.Read
 
 main :: IO ()
 main = do
-
-    let answer = ""
-    putStrLn "Are you ready to play? yes/no: "
-    answer <- getLine
+    menu 0
     
-    if answer == "yes" then
-         do putStrLn ""
-            putStrLn "************************* NEW GAME STARTING *************************"
-            let playerCards = [] :: [Card]
-            let dealerCards = [] :: [Card]
-            let unshuffledDeck = createDeck
-            deck <- shuffleDeck [] unshuffledDeck
-            let action = Hit :: Action
-            game dealerCards playerCards deck action
-        else exitSuccess
+menu :: Double -> IO()
+menu balance = do
+
+    putStrLn ""
+    putStrLn "++++++++++++++++++++++++++++ MENU +++++++++++++++++++++++++++++++"
+    putStrLn "Your Balance: "
+    print balance
+    putStrLn ""
+
+    if balance == 0
+        then do
+            putStrLn "Deposit more money: "
+            input <- getLine
+            let uptdBalance = (read input :: Double)
+            menu uptdBalance
+
+        else do
+            let answer = ""
+            putStrLn "Ready to play a hand ? yes/no: "
+            answer <- getLine
+            putStrLn ""
+            
+            if answer == "yes" then
+                do 
+                    putStrLn "Place bet: "
+                    input <- getLine 
+                    let bet = (read input :: Double)
+                    putStrLn ""
+
+                    if bet > balance
+                        then do
+                            putStrLn "not enough balance!"
+                            putStrLn ""
+                            menu balance
+                        else do
+                            let newBalance = balance - bet
+
+                            putStrLn ""
+                            putStrLn "************************* NEW ROUND *************************"
+
+                            let playerCards = [] :: [Card]
+                            let dealerCards = [] :: [Card]
+                            let unshuffledDeck = createDeck
+                            deck <- shuffleDeck [] unshuffledDeck
+                            let action = Hit :: Action
+                            game dealerCards playerCards deck action bet newBalance
+
+                else exitSuccess
 
 --showScores function handles the monitoring of game situation
-showScores :: [Card] -> [Card] -> Bool -> IO()
-showScores playerhand dealerhand last = do
+showScores :: [Card] -> [Card] -> Bool -> Double -> Double -> IO()
+showScores playerhand dealerhand last bet balance = do
     
     putStrLn "------------------------------------------------------------------------"
 
-    let playerScore = handScore ( handValues playerhand )
+    let playerScore = handScore $ handValues playerhand 
     let playercards = showHand playerhand
-    let dealerScoreSecret = handScore ( handValues (drop 1 dealerhand ))
-    let dealerScore = handScore ( handValues dealerhand )
+    let dealerScoreSecret = handScore $ handValues (drop 1 dealerhand )
+    let dealerScore = handScore $ handValues dealerhand
     let dealercards = showHand dealerhand
 
     if last == True
+
         then do
-            putStrLn $ ("Dealer's hand: | " ++ dealercards) 
+            putStrLn $ "Dealer's hand: | " ++ dealercards
             print dealerScore
             putStrLn $ "Your hand: | " ++ playercards
             print playerScore
+            putStrLn ""
 
             if dealerScore <= 21
                 then do
-                    print $ determineWinner dealerhand playerhand
-                    putStrLn ""
-                    main
+                    let result = determineWinner dealerhand playerhand
+                    print result
+                    if result == "You lost" 
+                        then do 
+                            putStrLn ""
+                            menu balance
+                        else if result == "You won"
+                            then do
+                                let newBalance = balance + bet*2
+                                menu newBalance
+                        else menu balance
                 else do
                     putStrLn "Dealer went over 21! You won!"
                     putStrLn ""
-                    main
+                    let newBalance = balance + bet*2
+                    menu newBalance
         else do
             let dealercardsSecret = showHand (drop 1 dealerhand)
             putStrLn $ "Dealer's hand: | secret card | " ++ dealercardsSecret
@@ -63,20 +109,21 @@ showScores playerhand dealerhand last = do
 
             if playerScore == 21 
                 then do 
-                    putStrLn "BlackJack!"
+                    putStrLn "You hit 21!"
                     print $ determineWinner dealerhand playerhand
                     putStrLn ""
-                    main
+                    let newBalance = balance + bet*3
+                    menu newBalance
                 else if playerScore > 21
                     then do
                         putStrLn "You went over 21! Game lost."
                         putStrLn ""
-                        main
+                        menu balance
                 else putStrLn ""
 
 --game function that handles the game logic
-game :: [Card] -> [Card] -> Deck -> Action -> IO()
-game dealerHand playerHand deck action = do
+game :: [Card] -> [Card] -> Deck -> Action -> Double -> Double -> IO()
+game dealerHand playerHand deck action bet balance = do
     
     if dealerHand == [] && playerHand == [] && length dealerHand == 0
         then do
@@ -85,15 +132,15 @@ game dealerHand playerHand deck action = do
             let deckAfter = snd draw
             let pHand = [(cards !! 0), (cards !! 2)]
             let dHand = [(cards !! 1), (cards !! 3)]
-            showScores pHand dHand False
+            showScores pHand dHand False bet balance
 
             putStrLn "Enter next action hit/stay: "
             actionString <- getLine 
             putStrLn ""
 
             if actionString == "hit" 
-                then game dHand pHand deckAfter Hit
-                else game dHand pHand deckAfter Stay
+                then game dHand pHand deckAfter Hit bet balance
+                else game dHand pHand deckAfter Stay bet balance
 
         else if action == Hit && handScore (handValues playerHand) < 21 
             then do
@@ -101,15 +148,15 @@ game dealerHand playerHand deck action = do
                 let cards = fst draw
                 let deckAfter = snd draw
                 let pHand = playerHand ++ cards
-                showScores pHand dealerHand False
+                showScores pHand dealerHand False bet balance
 
                 putStrLn ""
                 putStrLn "Enter next action hit/stay: "
                 actionString <- getLine
                 putStrLn ""
 
-                if actionString == "hit" then game dealerHand pHand deckAfter Hit 
-                else game dealerHand pHand deckAfter Stay
+                if actionString == "hit" then game dealerHand pHand deckAfter Hit bet balance
+                else game dealerHand pHand deckAfter Stay bet balance
 
         else if action == Stay
             then do
@@ -119,8 +166,8 @@ game dealerHand playerHand deck action = do
                         let card = fst draw
                         let deckAfter = snd draw
                         let dHand = dealerHand ++ card
-                        game dHand playerHand deckAfter Stay
+                        game dHand playerHand deckAfter Stay bet balance
                     else do
-                        showScores playerHand dealerHand True
+                        showScores playerHand dealerHand True bet balance
         else putStrLn "Program has ended."
 
